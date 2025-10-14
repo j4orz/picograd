035381101c229fc8b46199bce8a6a3b321849ddb
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-10-14 05:26:57 -0400

    add datasets, numpy, and matplotlib to examples

--- examples/1_elements.py
+++ examples/1_elements.py
@@ -1,28 +1,29 @@
-import picograd
-import matplotlib.pyplot as plt
+# import numpy as np
+# import picograd
+# import matplotlib.pyplot as plt
+# import pandas as pd
 
-def tensor_on_het():
-    print("tensors on heterogeneous runtimes: picograd.Tensor")
+def learn_regression():
+    print("learning gaussisans with regressin: price prediction")
 
 def vecs_as_probs():
-    print("vectors as probabilities: picograd.distributions")
+    print("vectors as probabilities")
 
 def mat_as_data_and_fn():
     print("matrices as data and functions: picograd.linalg")
 
+def ten_on_parallel_processors():
+    print("tensors on parallel processors")
+
 def matcalc_for_opt():
     print("matrix calculus for optimization: picograd.optim")
 
-def learn_gaussian_with_regression():
-    print("learning gaussisans with regressin: price prediction")
-
 def main():
-    print("sitp ch1: elements of learning") # read ch1.0: https://j4orz.ai/sitp/1.0
-    tensor_on_het()                      # read ch1.1: https://j4orz.ai/sitp/1.1
-    vecs_as_probs()                      # read ch1.2: https://j4orz.ai/sitp/1.2
-    mat_as_data_and_fn()                 # read ch1.3: https://j4orz.ai/sitp/1.3
-    matcalc_for_opt()                    # read ch1.4: https://j4orz.ai/sitp/1.4
-    learn_gaussian_with_regression()     # read ch1.5: https://j4orz.ai/sitp/1.5
+    print("sitp ch1: elements of learning")
+    learn_regression()
+    # vecs_as_probs()
+    # mat_as_data_and_fn()
+    # matcalc_for_opt()
 
 if __name__ == "__main__":
     main()
--- examples/3_autodiff.py
+++ examples/3_autodiff.py
@@ -14,11 +14,11 @@ def muon():
     print("muon")
 
 def main():
-    print("sitp ch3: anatomy of the autograd") # read ch3.0: https://j4orz.ai/sitp/3.0
-    ad()                                 # read ch3.1: https://j4orz.ai/sitp/3.1
-    sgd()                                # read ch3.2: https://j4orz.ai/sitp/3.2
-    adam()                               # read ch3.3: https://j4orz.ai/sitp/3.3
-    muon()                               # read ch3.4: https://j4orz.ai/sitp/3.4
+    print("sitp ch3: anatomy of the autograd")
+    ad()
+    sgd()
+    adam()
+    muon()
 
 if __name__ == "__main__":
     main()
