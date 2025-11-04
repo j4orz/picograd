43796e049eb225f9c2dd093a72ccfa09f237db09
j4orz <zhang.jeffreyd@gmail.com>
2025-11-04 15:13:36 +0000

    sketch runtime architecture with vendor runtime, program, allocator, and compiler

---
+++ python/picograd/device.py
@@ -0,0 +1,30 @@
+class Allocator:
+  def __init__(self): raise NotImplementedError("todo")
+
+class VendorCompiler:
+  def __init__(self, cachekey:str|None=None): self.cachekey = None if DISABLE_COMPILER_CACHE else cachekey
+  def compile(self, src:str) -> bytes: return src.encode()   # NOTE: empty compiler is the default
+  def compile_cached(self, src:str) -> bytes:
+    if self.cachekey is None or (lib := diskcache_get(self.cachekey, src)) is None:
+      assert not getenv("ASSERT_COMPILE"), f"tried to compile with ASSERT_COMPILE set\n{src}"
+      lib = self.compile(src)
+      if self.cachekey is not None: diskcache_put(self.cachekey, src, lib)
+    return lib
+  def disassemble(self, lib:bytes): pass
+  def __init__(self): raise NotImplementedError("todo")
+
+class VendorRuntime:
+  def __init__(self, device:str, allocator:Allocator): raise NotImplementedError("todo")
+
+# class TinyHCQRuntime:
+#   def __init__(self, device:str, allocator:Allocator): raise NotImplementedError("todo")
+
+class Device:
+  """
+  picograd's device runtimes follow the tinygrad runtime architecture where a
+  VendorRuntime manages compute with a Program and memory with an Allocator
+  """
+  def __init__(self): raise NotImplementedError("todo")
+
+
+ALL_DEVICES = ["CPU", "HIP"]
