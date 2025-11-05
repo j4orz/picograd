b9fbf879b4030688a357402111f77ed12ffd01a9
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-11-05 07:59:15 -0500

    move nn.py into directory with optim.py

--- python/picograd/__init__.py
+++ python/picograd/__init__.py
@@ -3,7 +3,8 @@ print("initializing picograd")
 print("importing picograd._pgrs")
 _pgrs = _import_module("picograd._pgrs")
 
-from . import nn
+from . import optim
+from .nn import nn
 from .tensor import Tensor # language
 from .device import Device # runtime
-__all__ = ["nn", "Tensor", "Device"]
+__all__ = ["optim", "nn", "Tensor", "Device"]
---
+++ python/picograd/nn/optim.py
@@ -0,0 +1,7 @@
+class Optimizer:
+  def __init__(): raise NotImplementedError("todo")
+  def step(): raise NotImplementedError("todo")
+
+# GD
+# SGD
+# ADAM
