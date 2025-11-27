59eafeeb7b50d055174fc57f0bc8d83d668926d7
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-11-27 13:29:24 -0500

    wholesale import tinygrad's buffer methods on Op

--- python/picograd/op.py
+++ python/picograd/op.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-from typing import Callable, Sequence
-import math, ctypes
+from typing import Callable
+import ctypes
 from dataclasses import dataclass
 from enum import auto, IntEnum, Enum
 from picograd.device import Allocator
@@ -8,6 +8,12 @@ from picograd.dtype import DType, dtypes
 from picograd.helpers import DEBUG, MAX_BUFFER_SIZE
 
 sint = int # |Op MOOSE
+
+class PatternMatcher:
+  def __init__(): raise NotImplementedError
+
+class Pattern:
+  def __init__(): raise NotImplementedError
 
 class FastEnum(IntEnum): # wrapper around IntEnum that preserves Enum.__str__ and makes auto() unique across all FastEnum subclasses
   def __str__(self): return Enum.__str__(
@@ -17,32 +23,14 @@ class FastEnum(IntEnum): # wrapper around IntEnum that preserves Enum.__str__ an
 
 # the order of these OpCode controls the order of the toposort
 class OpCode(FastEnum):
-  # ops that aren't rendered
-  # NOOP = auto(); SINK = auto(); UNIQUE = auto(); DEVICE = auto(); KERNEL = auto(); PRECAST = auto(); REWRITE_ERROR = auto() 
-  # SENTINEL = auto()
-  # AFTER = auto() # AFTER passes src[0] through and promises in the toposort that any consumers of the AFTER run after src[1:]
-  # GROUP = auto() # GROUP is a NOOP that just merges things together
-
-  # buffer ops
-  # COPY = auto(); BUFFER = auto(); BUFFER_VIEW = auto(); MSELECT = auto(); MSTACK = auto()
-  # BUFFERIZE = auto() # create buffer
-  # CONTIGUOUS = auto(); CONTIGUOUS_BACKWARD = auto(); DETACH = auto() # ops that adjust the behavior of the scheduler
-
-  # movement ops! these only exist in the tensor graph
-  # RESHAPE = auto(); PERMUTE = auto(); EXPAND = auto(); PAD = auto(); SHRINK = auto(); FLIP = auto()
-  # MULTI = auto()  # MULTI is really a movement op
-
-  # DEFINE_GLOBAL = auto(); DEFINE_LOCAL = auto(); DEFINE_REG = auto() # TODO: unify these ops into the levels of the memory hierarchy. depends on ASSIGN is STORE
-  # DEFINE_VAR = auto(); BIND = auto() # this is for symbolic shapes
-  # SPECIAL = auto()   # this is a RANGE for GPU dimensions, similar to symbolic shapes but not exactly
-
-  # REDUCE_AXIS = auto(); REDUCE = auto(); ALLREDUCE = auto() # reduce
-  # UNROLL = auto(); CONTRACT = auto(); GEP = auto(); VECTORIZE = auto(); CAT = auto(); PTRCAT = auto() # optimization helper ops
+  # ops that aren't rendered: noop, sink, unique, device, kernel, precast, rewrite_error, sentinel, after, group
+  # buffer ops: copy, buffer, buffer_view, mselect, mstack, bufferize, contiguous, contiguous_backward
+  # movement ops: these only exist in the tensor graph reshape, permute, expand, pad, shrink, flip, multi
+  # def_global, def_local, def_reg, def_var, bind, special(range)
+  # reduce_axis, reduce, allreduce
+  # unroll, contract, gep, vectorize, cat, ptrcat
   CAST = auto(); BITCAST = auto(); EXP2 = auto(); LOG2 = auto(); SIN = auto(); SQRT = auto(); RECIPROCAL = auto(); NEG = auto(); TRUNC = auto() # unaryops
-  # LOAD = auto(); STORE = auto() # load/store before math
-  # ASSIGN = auto()  # TODO: ASSIGN is STORE, remove ASSIGN
-  # WMMA = auto()   # tensor core math op, not elementwise
-  # INDEX = auto() # INDEX is a BinaryOp similar to ADD, but it operates on pointers
+  # laod, store, assign, wmma, index
 
   # binaryops
   MM = auto(); FA = auto() # TODO: order??
@@ -51,13 +39,12 @@ class OpCode(FastEnum):
   XOR = auto(); OR = auto(); AND = auto()
   THREEFRY = auto(); SUB = auto(); FDIV = auto(); POW = auto()
 
-  # WHERE = auto(); MULACC = auto() # ternaryops
-  # BARRIER = auto(); RANGE = auto(); IF = auto(); END = auto(); ENDIF = auto() # control flow ops
-  # VCONST = auto(); CONST = auto() # consts. VCONST is a vectorized const
-  # CUSTOM = auto(); CUSTOMI = auto() # CUSTOM/CUSTOMI are used to output strings into codegen. the I makes the string inline
+  # where, mulacc
+  # barrier, range, if, end, endif
+  # vconst, const, custom, customi
 
 @dataclass(eq=False, slots=True)
-class Op: # (ComputeMixin): # MovementMixin, metaclass=UOpMetaClass
+class Op: # (ComputeMixin): # MovementMixin, metaclass=OpMetaClass
   """
   ...
   """
@@ -81,12 +68,81 @@ class Op: # (ComputeMixin): # MovementMixin, metaclass=UOpMetaClass
           for s in reversed(node.src): stack.append((s, False)) # push srcs on the stack
       else: visited[node] = None # second time i'm seeing this node, add it to returned toposort
     return visited
+  
+  @staticmethod
+  def new_buffer(device:str|tuple[str, ...], size:int, dtype:DType, num=None):
+    return Op(OpCode.BUFFER, dtype, (Op.unique(num), Op(OpCode.DEVICE, arg=device)), size)
+  
+  @property
+  def device(self) -> str|tuple[str, ...]: return unwrap(self._device)
+  @recursive_property
+  def _device(self) -> str|tuple[str, ...]|None:
+    if self.op is OpCode.DEVICE: return self.arg
+    if self.op is OpCode.BUFFERIZE: return self.arg.device
+    if self.op is OpCode.AFTER: return self.src[0]._device
+    if self.op is OpCode.MSELECT:
+      assert isinstance(self.src[0].device, tuple), "mselect must be on tuple device"
+      return self.src[0].device[self.arg]
+    if self.op is OpCode.MSTACK: return tuple(cast(str, x.device) for x in self.src)
+    if self.op in {OpCode.COPY, OpCode.BUFFER, OpCode.ALLREDUCE}: return self.src[1].device
+    for x in self.src:
+      if x._device is not None: return x._device
+    return None
+  @property
+  def buf_Op(self) -> Op:
+    if self.op is OpCode.BUFFER: return self
+    if self.op is OpCode.MSELECT: return self.src[0].buf_Op.mselect(self.arg)
+    if self.op is OpCode.MSTACK: return Op(OpCode.MSTACK, self.dtype, src=tuple(x.buf_Op for x in self.src))
+    assert self.base.op is OpCode.AFTER, f"must be AFTER {self.base.op}"
+    return self.base.src[0].buf_Op.base
 
-class PatternMatcher:
-  def __init__(): raise NotImplementedError
+  def as_buf(self) -> Op:
+    if self.op is OpCode.MSELECT: return self.src[0].as_buf().mselect(self.arg)
+    if self.op is OpCode.MSTACK: return Op(OpCode.MSTACK, self.dtype, src=tuple(x.as_buf() for x in self.src))
+    # TODO: this should be the only one of these. this is the one RANGEIFY uses
+    s = self
+    while len(s.src) and s.op not in {OpCode.BUFFER, OpCode.BUFFERIZE, OpCode.MSTACK}: s = s.src[0]
+    return s
 
-class Pattern:
-  def __init__(): raise NotImplementedError
+  def buf_target(self) -> Op:
+    # the buffer that's being loaded from or store to
+    match self.op:
+      case OpCode.DEFINE_GLOBAL | OpCode.DEFINE_LOCAL | OpCode.DEFINE_REG: return self
+      case OpCode.AFTER | OpCode.INDEX | OpCode.STORE | OpCode.LOAD: return self.src[0].buf_target()
+      case OpCode.VECTORIZE:
+        assert all_same(self.src)
+        return self.src[0].buf_target()
+      case _: raise RuntimeError(f"buf_target called on non load/index/store {self.op}")
+
+  @property
+  def buffer(self) -> Buffer|MultiBuffer:
+    from tinygrad.device import Buffer, MultiBuffer
+    if self is not self.base:
+      assert self.op is OpCode.RESHAPE, f"can only be RESHAPE {self}"
+      return self.src[0].buffer
+    if self.op is OpCode.MSELECT:
+      ret = self.src[0].buffer
+      assert isinstance(ret, MultiBuffer)
+      return ret.bufs[self.arg]
+    if self.op is OpCode.MSTACK:
+      ret = MultiBuffer.__new__(MultiBuffer)
+      ret.bufs = [cast(Buffer, x.buffer) for x in self.src]
+      assert all_same([x.size for x in ret.bufs]) and all_same([x.dtype for x in ret.bufs]), "multibuffers mismatch buffers"
+      return ret
+    assert self.op is OpCode.BUFFER, f"must be BUFFER {self.op}"
+    if (cret:=buffers.get(self)) is not None: return cret
+    rdtype = self.dtype if isinstance(self.dtype, ImageDType) else self.dtype.base
+    if isinstance(self.device, tuple): ret = MultiBuffer(self.device, self.size, rdtype).ref(1)
+    else: ret = Buffer(self.device, self.size, rdtype).ref(1)
+    buffers[self] = ret
+    return ret
+  @property
+  def realized(self) -> Buffer|MultiBuffer|None:
+    # NOTE: this is used by the JIT to determine which inputs we capture
+    return self.buffer if self.op in {OpCode.BUFFER, OpCode.MSTACK} and self.buffer.is_allocated() else None
+  @property
+  def is_realized(self) -> bool:
+    return all(x.base.realized is not None for x in self.base.src) if self.base.op is OpCode.MULTI else self.base.realized is not None
 
 # **************** Python/C Foreign Function Helpers  ****************
 def from_mv(mv:memoryview, to_type:type[ctypes._SimpleCData]=ctypes.c_char) -> ctypes.Array:
@@ -102,14 +158,14 @@ class Buffer:
   def __init__(self,
                device:str, size:int, dtype:DType,
                opaque:Any=None, options:BufferSpec|None=None, initial_value:bytes|None=None,
-               uop_refcount=0, base:Buffer|None=None, offset:int=0, preallocate=False):
+               Op_refcount=0, base:Buffer|None=None, offset:int=0, preallocate=False):
     assert isinstance(dtype, DType) and not isinstance(dtype, PtrDType)
     self.device, self.size, self.dtype, self.options, self.offset, self.allocated_views = device, size, dtype, options, offset, 0
 
     if base is None:
       assert offset == 0, "base buffers can't have offset"
       self._base = None
-      self._uop_refcount = uop_refcount
+      self._Op_refcount = Op_refcount
 
       if opaque is not None: self.allocate(opaque)
       if initial_value is not None:
