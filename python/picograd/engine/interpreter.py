09040f7026dfcd359f76d2f7a9afcc2f4abda008
j4orz <zhang.jeffreyd@gmail.com>
2025-12-27 14:15:40 +0000

    sketch ExternalKernel struct for Interpreter

--- python/picograd/engine/interpreter.py
+++ python/picograd/engine/interpreter.py
@@ -1,9 +1,18 @@
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
+import pathlib
 
-if TYPE_CHECKING:
-  from picograd.sugar.tensor import Tensor
+if TYPE_CHECKING: from picograd.sugar.tensor import Tensor
+from picograd.engine import OpCode, OpNode
 
-class Interpreter:
-  @staticmethod
-  def evaluate(t: Tensor) -> Tensor:
-    raise  NotImplementedError("todo")
+@dataclass(frozen=True)
+class ExternalKernel:
+  name: str; source: str|pathlib.Path; compiler: str = "nvcc"
+  globals: tuple[int, int, int] = (1, 1, 1); locals: tuple[int, int, int] = (1, 1, 1)
+  args: tuple[int, ...] = (); extra_args: tuple[str, ...] = ()
+shared_mem: int = 0
+
+# class Interpreter:
+#   @staticmethod
+#   def evaluate(t: Tensor) -> Tensor:
+#     raise  NotImplementedError("todo")
