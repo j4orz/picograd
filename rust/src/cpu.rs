9eb411076f5857ae4d7b03916c406937213f3d0f
jeffzh4ng <zhang.jeffreyd@gmail.com>
2026-02-13 09:09:18 -0500

    book: update 1.5 toc
    book: flatten part1 toc
    
    book: move autograd to part2
    
    book: clean up toc
    
    spec out gemm cpu acceleration: locality with loop reordering/blocking, ilp with unrolling/intrinsics, and tlp
    
    remove models from examples, not there yet

--- rust/src/cpu.rs
+++ rust/src/cpu.rs
@@ -1,19 +1,86 @@
 pub fn sgemm(
-  m: usize, n: usize, k: usize,
+  m: usize, n: usize, p: usize,
   alpha: f32, beta: f32,
-  a: &[f32], b: &[f32], c: &mut [f32]
-) {
-  for j in 0..n {
-    for p in 0..k {
-      for i in 0..m {
-        let idx = i * n + j;
-        let prod = alpha * a[i * k + p] * b[p * n + j];
-        if p == 0 {
-          c[idx] = beta * c[idx] + prod;
-        } else {
-          c[idx] += prod;
-        }
-      }
+  a: &[f32], b: &[f32], c: &mut [f32] ) {
+  for i in 0..m {
+    for j in 0..n {
+      let mut ab = 0.0f32;
+      for k in 0..p { ab += a[i*p+k] * b[k*n+j] }
+      let idx = i*n+j;
+      c[idx] = alpha * ab + beta * c[idx];
     }
   }
-} 
+}
+
+pub fn sgemm1( // temporal locality (loads/store): loop order
+  m: usize, n: usize, p: usize,
+  alpha: f32, beta: f32,
+  a: &[f32], b: &[f32], c: &mut [f32] ) {
+  for i in 0..m {
+    for j in 0..n {
+      let mut ab = 0.0f32;
+      for k in 0..p { ab += a[i*p+k] * b[k*n+j] }
+      let idx = i*n+j;
+      c[idx] = alpha * ab + beta * c[idx];
+    }
+  }
+}
+
+pub fn sgemm2( // spatial locality (loads/store): block on register and caches
+  m: usize, n: usize, p: usize,
+  alpha: f32, beta: f32,
+  a: &[f32], b: &[f32], c: &mut [f32] ) {
+  for i in 0..m {
+    for j in 0..n {
+      let mut ab = 0.0f32;
+      for k in 0..p { ab += a[i*p+k] * b[k*n+j] }
+      let idx = i*n+j;
+      c[idx] = alpha * ab + beta * c[idx];
+    }
+  }
+}
+
+pub fn sgemm3( // ilp loop unroll
+  m: usize, n: usize, p: usize,
+  alpha: f32, beta: f32,
+  a: &[f32], b: &[f32], c: &mut [f32] ) {
+  for i in 0..m {
+    for j in 0..n {
+      let mut ab = 0.0f32;
+      for k in 0..p { ab += a[i*p+k] * b[k*n+j] }
+      let idx = i*n+j;
+      c[idx] = alpha * ab + beta * c[idx];
+    }
+  }
+}
+
+// core::arch/asm! intrinsics/inline assembly
+// std::simd portable simd
+// autov11n
+pub fn sgemm4( // dlp microkernel (avx/sve) matrix extensions??
+  m: usize, n: usize, p: usize,
+  alpha: f32, beta: f32,
+  a: &[f32], b: &[f32], c: &mut [f32] ) {
+  for i in 0..m {
+    for j in 0..n {
+      let mut ab = 0.0f32;
+      for k in 0..p { ab += a[i*p+k] * b[k*n+j] }
+      let idx = i*n+j;
+      c[idx] = alpha * ab + beta * c[idx];
+    }
+  }
+}
+
+pub fn sgemm5( // tlp todo!
+  m: usize, n: usize, p: usize,
+  alpha: f32, beta: f32,
+  a: &[f32], b: &[f32], c: &mut [f32] ) {
+  for i in 0..m {
+    for j in 0..n {
+      let mut ab = 0.0f32;
+      for k in 0..p { ab += a[i*p+k] * b[k*n+j] }
+      let idx = i*n+j;
+      c[idx] = alpha * ab + beta * c[idx];
+    }
+  }
+}
